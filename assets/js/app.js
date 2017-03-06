266b2885f1252d97e1ecc51d426aeafff10d63a7
Neiza Nunez <neizapf@gmail.com>
2017-03-05 23:51:03 -0500

    Primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener('click', function (){
+  var button1 = document.getElementById('1');
+  var button1 = document.getElementById('2');
+  var button1 = document.getElementById('3');
+  var button1 = document.getElementById('4');
+  var button1 = document.getElementById('5');
+  var button1 = document.getElementById('6');
+  var button1 = document.getElementById('7');
+  var button1 = document.getElementById('8');
+  var button1 = document.getElementById('9');
+
+  })
+
+}
